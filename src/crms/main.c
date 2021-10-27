dc6a0561f9c53c10f983a016d30dc96acf90a6d2
Francisco Balmaceda <francisco.balmaceda@uc.cl>
2021-10-27 11:22:28 -0300

    main2 llenado y vaciado de processos

--- src/crms/main.c
+++ src/crms/main.c
@@ -12,50 +12,29 @@ int main(int argc, char const *argv[])
         cr_mount(path);
         printf("Path cr_mount: %s\n", path);
     
+        printf("\nFuncion cr_start_process\n");
+        for (size_t i = 0; i < 16; i++)
+        {
+            char* process_name_start = "new_process";
+            cr_start_process(i, process_name_start);
+        }
+        
         printf("\nFuncion cr_ls_processes\n");
         cr_ls_processes();
-
-
-        int process_id_files = 400;
-        printf("\nFuncion cr_ls_files invalid\n");
-        cr_ls_files(process_id_files);
-
-        process_id_files = 0;
-        printf("\nFuncion cr_ls_files valid\n");
-        cr_ls_files(process_id_files);
-
-        printf("\nFuncion cr_exists\n");
-        int process_id_exists = 0;
-        char* file_name_exists = "secret.txt";
-        char* file_name_exists_not = "no.mp4";
-        int exists = cr_exists(process_id_exists, file_name_exists);
-        int exists_not = cr_exists(process_id_exists, file_name_exists_not);
-        printf("retorno: %i\n", exists);
-        printf("retorno de inexitente: %i\n", exists_not);
         
+        printf("\nCreando processo con memoria llena\n");
+        cr_start_process(16, "nuevo_fake");
 
-        // printf("\nFuncion cr_start_process\n");
-        // int process_id_start = 202;
-        // char* process_name_start = "new_process";
-        // cr_start_process(process_id_start, process_name_start);
-        // // int process_id_start_invalid = 100;
-        // // char* process_name_start_invalid = "new_process";
-        // // cr_start_process(process_id_start_invalid, process_name_start_invalid);
-        // for (size_t i = 0; i < 17; i++)
-        // {
-        //     /* code */
-        // }
-        
+        printf("\nFuncion process_id_finish\n");
+        for (size_t i = 0; i < 16; i++)
+        {
+            cr_finish_process(i);
+        }
+        printf("\nerror porque ya se elimino processo id2\n");
+        cr_finish_process(2);
 
-        // printf("\nFuncion cr_ls_processes\n");
-        // cr_ls_processes();
-
-        // printf("\nFuncion process_id_finish\n");
-        // int process_id_finish = 202;
-        // cr_finish_process(process_id_finish);
-
-        // printf("\nFuncion cr_ls_processes\n");
-        // cr_ls_processes();
+        printf("\nFuncion cr_ls_processes con error\n");
+        cr_ls_processes();
     }
     return 0;
 }
